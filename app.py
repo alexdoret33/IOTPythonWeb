06c0df5b968b056598e0b759b379686a5d3fc273
Alexandre DORET <alexandre.doret@ynov.com>
2018-04-09 14:11:35 +0200

    MAJ

--- app.py
+++ app.py
@@ -26,35 +26,29 @@ def get_db () :
         g.db = connect_db()
     return g.db
 
-@app.route('/show-entries/')
-def show_entries () :
-    db = get_db()
-    db.execute('SELECT name, value FROM entries')
-    entries = db.fetchall()
-    return render_template('show-entries.html', entries = entries)
 
 @app.route('/')
 def index () :
     return 'Bienvenue'
 
+
 @app.route('/monitoring/')
 def monitoring () :
-    page = """ 
-        <!doctype html>
-        <html lang="fr">
-        <head>
-                <meta charset="utf-8">
-                <title>Monitoring</title>
-        </head>
-        <body>
-                <h1>Monitoring de Sites Web</h1>
-                <p>
-                        
-                </p>
-        </body>
-        </html> 
-    """
-    return page
+	return render_template('monitoring.html')
+
+
+@app.route('/admin/')
+def admin () :
+    if not session.get('user') or not session.get('user')[2] :
+        return redirect(url_for('login'))
+
+    return render_template('admin.html', user = session['user'])
+
+
+@app.route('/admin/logout/')
+def admin_logout () :
+    session.clear()
+    return redirect(url_for('login'))
 
 
 @app.route('/login/', methods = ['GET', 'POST'])
@@ -77,17 +71,6 @@ def login () :
 
     return render_template('login.html')
 
-@app.route('/admin/')
-def admin () :
-    if not session.get('user') or not session.get('user')[2] :
-        return redirect(url_for('login'))
-
-    return render_template('admin.html', user = session['user'])
-
-@app.route('/admin/logout/')
-def admin_logout () :
-    session.clear()
-    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
